135c5e542c6f61f42206121bbf787fa3ca57061a
ruah0807 <kimhk0315@gmail.com>
2024-10-22 17:40:24 +0900

    spinner 챗봇 응답 안쪽에 넣기

--- chatbot/steam.py
+++ chatbot/steam.py
@@ -50,7 +50,7 @@ st.title("💬🤖 Nocoding AI Chatbot ")
 # st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Please choose language, 'English' or '한국어'"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Please choose language, 'English' or '한국어' or other languagues"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -65,38 +65,43 @@ if prompt := st.chat_input():
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+    place_holder = st.chat_message("assistant")
 
-    response = client.beta.threads.messages.create(
-        thread_id=st.session_state["thread_id"],
-        role='user',
-        content=prompt
-    )
-    # print(response)
+    with place_holder: 
+        with st.spinner("응답을 기다리는 중..."):
+        
+            response = client.beta.threads.messages.create(
+                thread_id=st.session_state["thread_id"],
+                role='user',
+                content=prompt
+            )
+            # print(response)
 
-    run = client.beta.threads.runs.create(
-        thread_id= st.session_state["thread_id"],
-        assistant_id=ass_id,
-    )
-    run_id = run.id
+            run = client.beta.threads.runs.create(
+                thread_id= st.session_state["thread_id"],
+                assistant_id=ass_id,
+            )
+            run_id = run.id
 
-    while True:
-        run = client.beta.threads.runs.retrieve(
-            thread_id=st.session_state["thread_id"],
-            run_id=run_id
-        )
-        if run.status == "completed":
-            break
-        else:
-            time.sleep(2)
-        print(run.status)
+            while True:
+                run = client.beta.threads.runs.retrieve(
+                    thread_id=st.session_state["thread_id"],
+                    run_id=run_id
+                )
+                if run.status == "completed":
+                    break
+                else:
+                    time.sleep(2)
+                print(run.status)
 
-    thread_messages = client.beta.threads.messages.list(st.session_state["thread_id"])
-    # print(thread_messages.data)
+            thread_messages = client.beta.threads.messages.list(st.session_state["thread_id"])
+            # print(thread_messages.data)
 
-    msg = thread_messages.data[0].content[0].text.value
-    print(msg)
+            msg = thread_messages.data[0].content[0].text.value
+            print(msg)
 
-    st.session_state.messages.append({"role": "assistant", "content": msg})  
-    st.chat_message("assistant").write(msg)
+            # 로딩메세지를 실제 응답으로 교체
+            place_holder.write(msg)
+            st.session_state.messages.append({"role": "assistant", "content": msg})  
 
 
